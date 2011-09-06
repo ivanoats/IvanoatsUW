b481d860d75f5b7db0c94322dd6517d687f54426
Ivan Storck <ivanoats@gmail.com>
2011-09-06 14:22:30 -0700

    changed url

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/ivanoats/"
+http_images_path = "/ivanoats/images"
+http_fonts_path = "/ivanoats/fonts"
+css_dir = "public/ivanoats/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
